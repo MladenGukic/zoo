3e73bfa33501e8e7a8907b2bfccc132846174f7a
MladenGukic <mladengukic@gmail.com>
2019-11-19 20:59:00 +0100

    Napravljena komponenta 'AnimalList'. Postavljen niz objekata koji predstavlja zivotinje. Zivotinje prikazane u tabeli. Ako zivotinja nema datum rodjenja, u tabeli ispisano 'Nepoznato'.

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="index" v-for="(value, index) in animals">
+          <td>{{value.species}}</td>
+          <td>{{value.name}}</td>
+          <td v-if="!value.birth">Nepoznato</td>
+          <td v-else>{{value.birth.getFullYear()}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data() {
+    return {
+      animals: [
+        {species: 'mamml', name: 'King', birth: new Date()},
+        {species: 'bird', name: 'Ko-ko', birth: new Date()},
+        {species: 'mamml', name: 'Cleopatra', birth: new Date()},
+        {species: 'fish', name: 'Joe'},
+        {species: 'bird', name: 'Cleopatra', birth: new Date()}
+      ]
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
