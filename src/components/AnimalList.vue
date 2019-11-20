733a00fa4e37414167a1a25e6977de79c7378077
MladenGukic <mladengukic@gmail.com>
2019-11-21 00:34:52 +0100

    Dodati sektori, u listi se ispisuje ime sektrora u kojoj se nalazi zivotinja

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -4,6 +4,9 @@
       <input placeholder="Specie" id="species" type="text" name="species" v-model="form.species"/>
       <input placeholder="Name" id="name" type="text" name="name" v-model="form.name" />
       <input placeholder="Birth" id="birth" type="hiden" name="birth" v-model="form.birth"/>
+      <select v-model="form.selectedSector">
+        <option v-bind:key="index" v-for="(sector, index) in sectors">{{sector}}</option>
+      </select>      
       <button type="submit"> Add Animal </button>
     </form>
   <div>
@@ -13,17 +16,23 @@
           <th>Species</th>
           <th>Name</th>
           <th>Birth</th>
+          <th>Sector</th>
         </tr>
       </thead>
       <tbody>
         <tr v-bind:key="index" v-for="(value, index) in animals">
-          <button @click="move(index)">Move to top </button>
           <td>{{value.species}}</td>
           <td>{{value.name}}</td>
-          <td v-if="!value.birth">Nepoznato</td>
+          <td v-if="!value.birth">Unknown</td>
           <td v-else-if="!value.birth.getFullYear">{{value.birth}}</td>
           <td v-else>{{value.birth.getFullYear()}}</td>
+          <td>{{value.selectedSector}}</td>
+          <td>
+          <button @click="move(index)">Move to top </button>
+          </td>
+          <td>
           <button @click="remove(index)">Remove</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -37,15 +46,18 @@ export default {
   data() {
     return {
       animals: [
-        {species: 'mamml', name: 'Dog', birth: new Date()},
+        {species: 'mammal', name: 'Dog', birth: new Date()},
         {species: 'bird', name: 'Parrot', birth: new Date()},
-        {species: 'mamml', name: 'Cat', birth: new Date()},
+        {species: 'mammal', name: 'Cat', birth: new Date()},
         {species: 'fish', name: 'Tuna'},
         {species: 'bird', name: 'Pidgeon', birth: new Date()}
       ],
 
-      form: {species: '', name: '', birth: ''}
-    };
+      form: {species: '', name: '', birth: '', selectedSector: ''},
+
+      sectors: ['Snake', 'Bird', 'Mammal']
+    }
+    
   }, 
 
   methods: {
